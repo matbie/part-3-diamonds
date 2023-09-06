30297ace0725168cb29ec28278321e1be80d69b0
Mateusz Bieniak <mateusz.bieniak@linux.pl>
2023-09-06 22:29:08 +0200

    added complete site

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
-const footerItem = document.querySelector('.footer')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
-	footerItem.classList.toggle('footer--active')
 }
 
 navBtn.addEventListener('click', handleNav)
